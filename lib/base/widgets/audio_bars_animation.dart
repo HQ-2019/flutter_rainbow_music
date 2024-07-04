3f8753fd861c4f4990eb2be2fa487f155fca0153
hq <>
2024-07-05 02:05:43 +0800

    优化动效的流畅度

--- lib/base/widgets/audio_bars_animation.dart
+++ lib/base/widgets/audio_bars_animation.dart
@@ -10,13 +10,15 @@ class AudioBarsAnimation extends StatefulWidget {
       this.itemWidth = 3.0,
       this.itemSpace = 2.0,
       this.minHeight = 5.0,
-      this.maxHeight = 20});
+      this.maxHeight = 20,
+      this.duration = const Duration(milliseconds: 300)});
 
   final int itemCount;
   final double itemWidth;
   final double itemSpace;
   final double maxHeight;
   final double minHeight;
+  final Duration duration;
 
   @override
   State<StatefulWidget> createState() => _AudioBarsAnimationState();
@@ -24,55 +26,61 @@ class AudioBarsAnimation extends StatefulWidget {
 
 class _AudioBarsAnimationState extends State<AudioBarsAnimation>
     with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+  late List<AnimationController> _controllers;
   late List<Animation<double>> _animations;
-  late List<double> _currentHeights;
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
+  late List<double> _heights;
 
   @override
   void initState() {
     super.initState();
 
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 300),
-    )..addStatusListener((status) {
-        if (status == AnimationStatus.completed ||
-            status == AnimationStatus.dismissed) {
-          _generateAnimations();
-          _controller.forward(from: 0.0);
-        }
-      });
+    _heights = List.generate(widget.itemCount, (_) => _getRandomHeight());
 
-    _currentHeights = List.generate(widget.itemCount, (index) => 10.0);
-    _generateAnimations();
-    _controller.forward();
+    _controllers = List.generate(widget.itemCount, (index) {
+      return AnimationController(
+        duration: widget.duration,
+        vsync: this,
+      )..addStatusListener((status) {
+          if (status == AnimationStatus.completed) {
+            setState(() {
+              double oldHeight = _animations[index].value;
+              double newHeight = _getRandomHeight();
+              _animations[index] = Tween<double>(
+                begin: oldHeight,
+                end: newHeight,
+              ).animate(_controllers[index]);
+            });
+            _controllers[index].forward(from: 0.0);
+          }
+        });
+    });
+
+    _animations = List.generate(widget.itemCount, (index) {
+      return Tween<double>(
+        begin: _heights[index],
+        end: _getRandomHeight(),
+      ).animate(_controllers[index])
+        ..addListener(() {
+          setState(() {});
+        });
+    });
+
+    for (var controller in _controllers) {
+      controller.forward();
+    }
   }
 
-  void _generateAnimations() {
-    final random = Random();
-    setState(() {
-      _animations = List.generate(widget.itemCount, (index) {
-        final endHeight =
-            random.nextDouble() * (widget.maxHeight - widget.minHeight) +
-                widget.minHeight;
-        final beginHeight = _currentHeights[index];
-        Animation<double> animation =
-            Tween<double>(begin: beginHeight, end: endHeight).animate(
-          CurvedAnimation(
-            parent: _controller,
-            curve: const Interval(0.0, 1.0, curve: Curves.easeInOut),
-          ),
-        );
-        _currentHeights[index] = endHeight;
-        return animation;
-      });
-    });
+  @override
+  void dispose() {
+    for (var controller in _controllers) {
+      controller.dispose();
+    }
+    super.dispose();
+  }
+
+  double _getRandomHeight() {
+    return widget.minHeight +
+        Random().nextDouble() * (widget.maxHeight - widget.minHeight);
   }
 
   @override
